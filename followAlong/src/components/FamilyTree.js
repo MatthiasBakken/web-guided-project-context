1dfbf67d74b8f2e190782cc45018fee7e9c22ac3
Andy Bolos <andybolos@MacBook-Pro-2.local>
2021-05-26 20:38:26 -0600

    lecture work-in-progress

--- followAlong/src/components/FamilyTree.js
+++ followAlong/src/components/FamilyTree.js
@@ -3,20 +3,20 @@ import React, { useContext } from "react";
 import Parents from "./Parents";
 import Siblings from "./Siblings";
 
-export default function FamilyTree(props) {
+export default function FamilyTree() {
   return (
     <section className="FamilyTree">
-      <h1>{props.family.familyName}</h1>
+      <h1>{family.familyName}</h1>
       <h2>Parents</h2>
 
       {/* Pass data down as a prop */}
-      <Parents family={props.family} />
+      <Parents family={family} />
 
       <div className="spacer" />
       <h2>Siblings</h2>
 
       {/* Siblings will consume data from Context using render props */}
-      <Siblings family={props.family} />
+      <Siblings family={family} />
     </section>
   );
 }
