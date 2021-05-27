6d14dadc84a9af0c9d4407c78c424dddb7c088a2
Andy Bolos <andybolos@MacBook-Pro-2.local>
2021-05-26 21:13:46 -0600

    lecture work-in-progress

--- followAlong/src/components/Siblings.js
+++ followAlong/src/components/Siblings.js
@@ -1,37 +1,37 @@
 import React from "react";
 import { FamilyContext } from '../App';
 // render props pattern - old blah garbage
-const Siblings = () => {
-  return (
-    <section className="parents">
-      <FamilyContext.Consumer>
-      {(value) => (
-        <>
-        {value.activeFamily.siblings.map((p) => (
-          <div className="person" key={p.name}>
-            <img width="150" src={p.img} alt={p.name} />
-            <strong>{p.name}</strong>
-          </div>
-        ))}
-        </>
-      )
-
-      }
-      </FamilyContext.Consumer>
-    </section>
-  );
-};
 // const Siblings = () => {
 //   return (
 //     <section className="parents">
-//       {family.siblings.map((p) => (
-//         <div className="person" key={p.name}>
-//           <img width="150" src={p.img} alt={p.name} />
-//           <strong>{p.name}</strong>
-//         </div>
-//       ))}
+//       <FamilyContext.Consumer>
+//       {(value) => (
+//         <>
+//         {value.activeFamily.siblings.map((p) => (
+//           <div className="person" key={p.name}>
+//             <img width="150" src={p.img} alt={p.name} />
+//             <strong>{p.name}</strong>
+//           </div>
+//         ))}
+//         </>
+//       )
+
+//       }
+//       </FamilyContext.Consumer>
 //     </section>
 //   );
 // };
+const Siblings = () => {
+  return (
+    <section className="parents">
+      {family.siblings.map((p) => (
+        <div className="person" key={p.name}>
+          <img width="150" src={p.img} alt={p.name} />
+          <strong>{p.name}</strong>
+        </div>
+      ))}
+    </section>
+  );
+};
 
 export default Siblings;
