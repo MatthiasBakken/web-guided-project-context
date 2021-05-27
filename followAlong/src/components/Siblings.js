fc357861176adfd6eb7a733d181c5cb94df99aaa
Andy Bolos <andybolos@MacBook-Pro-2.local>
2021-05-26 20:54:04 -0600

    lecture work-in-progress

--- followAlong/src/components/Siblings.js
+++ followAlong/src/components/Siblings.js
@@ -1,16 +1,37 @@
 import React from "react";
-
+import { FamilyContext } from '../App';
+// render props pattern - old blah garbage
 const Siblings = () => {
   return (
     <section className="parents">
-      {family.siblings.map((p) => (
-        <div className="person" key={p.name}>
-          <img width="150" src={p.img} alt={p.name} />
-          <strong>{p.name}</strong>
-        </div>
-      ))}
+      <FamilyContext.Consumer>
+      {(family) => (
+        <>
+        {family.siblings.map((p) => (
+          <div className="person" key={p.name}>
+            <img width="150" src={p.img} alt={p.name} />
+            <strong>{p.name}</strong>
+          </div>
+        ))}
+        </>
+      )
+
+      }
+      </FamilyContext.Consumer>
     </section>
   );
 };
+// const Siblings = () => {
+//   return (
+//     <section className="parents">
+//       {family.siblings.map((p) => (
+//         <div className="person" key={p.name}>
+//           <img width="150" src={p.img} alt={p.name} />
+//           <strong>{p.name}</strong>
+//         </div>
+//       ))}
+//     </section>
+//   );
+// };
 
 export default Siblings;
